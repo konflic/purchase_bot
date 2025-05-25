c393a289a39f20f065f3b600d065702044b60a82
konflic <konflic@mail.ru>
2025-05-25 12:11:19 +0300

    Localization, cmd names, ux improvements

--- update.sh
+++ update.sh
@@ -4,4 +4,10 @@ echo "Pull git repo"
 git pull
 
 echo "Rebuild and restart container"
-sudo docker-compose down && sudo docker-compose build && sudo docker-compose up -d
+docker-compose down && docker-compose build && docker-compose up -d
+
+echo "Delete old images"
+docker rmi $(docker images -q)
+
+echo "Current state"
+docker ps
