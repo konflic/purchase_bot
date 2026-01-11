d5730fc0cb7dba2ad806785ea483d77909ed3bd5
konflic <konflic@mail.ru>
2026-01-11 18:11:44 +0300

    improved buttons

--- update.sh
+++ update.sh
@@ -4,10 +4,7 @@ echo "Pull git repo"
 git pull
 
 echo "Rebuild and restart container"
-docker-compose down && docker-compose build && docker-compose up -d
-
-echo "Delete old images"
-docker rmi $(docker images -q)
+docker compose down && docker compose build && docker compose up -d
 
 echo "Current state"
 docker ps
